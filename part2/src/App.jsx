6b88ffd275ec643dc17058b3899970322334bafb
Vixito <carlosandres81182@gmail.com>
2025-06-05 12:26:02 -0500

    Exercise Part 2.7 Full Stack Open

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -7,6 +7,10 @@ const App = () => {
     const [newName, setNewName] = useState('')
     const addPerson = (event) => {
         event.preventDefault()
+        if (persons.some(person => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`)
+            return
+        }
         const personObject = {
             name: newName
         }
