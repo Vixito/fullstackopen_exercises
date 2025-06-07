e955f6f728b66308a65d6939a37b22f3d8f2eefa
Vixito <carlosandres81182@gmail.com>
2025-06-06 22:48:31 -0500

    Exercise Part 2.19 Full Stack Open

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -9,30 +9,35 @@ const Filter = ({ filter, handleFilter }) => {
     )
 }
 
-const CountriesList = ({ countries }) => {
+const CountryDetail = ({ country }) => {
+    if (!country) return null;
+    return (
+        <div>
+            <h2>{country.name.common}</h2>
+            <p>Capital: {country.capital?.[0]}</p>
+            <p>Area: {country.area}</p>
+            <ul>
+                {country.languages && Object.values(country.languages).map(language => (
+                    <li key={language}>{language}</li>
+                ))}
+            </ul>
+            <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="100" />
+        </div>
+    );
+}
+
+const CountriesList = ({ countries, handleShowCountry }) => {
     if (countries.length > 10) {
         return <div>Too many matches, specify another filter</div>
     } else if (countries.length === 1) {
-        const country = countries[0];
-        return (
-            <div>
-                <h2>{country.name.common}</h2>
-                <p>Capital: {country.capital?.[0]}</p>
-                <p>Area: {country.area}</p>
-                <ul>
-                    {country.languages && Object.values(country.languages).map(language => (
-                        <li key={language}>{language}</li>
-                    ))}
-                </ul>
-                <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="100" />
-            </div>
-        );
+        return <CountryDetail country={countries[0]}/>
     } else {
         return (
             <ul>
                 {countries.map(country => (
                     <li key={country.cca3}>
-                        {country.name.common}
+                        {country.name.common} {' '}
+                        <button onClick={() => handleShowCountry(country)}>show</button>
                     </li>
                 ))}
             </ul>
@@ -43,6 +48,7 @@ const CountriesList = ({ countries }) => {
 const App = () => {
     const [filter, setFilter] = useState('');
     const [countries, setCountries] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState(null);
 
     useEffect(() => {
         countriesService.getAll()
@@ -62,10 +68,19 @@ const App = () => {
         country.name.common.toLowerCase().includes(filter.toLowerCase())
     );
 
+    const handleShowCountry = (country) => {
+        setSelectedCountry(country);
+    }
+
     return (
         <div>
             <Filter filter={filter} handleFilter={handleFilter}/>
-            {filter && <CountriesList countries={filteredCountries} />}
+            {filter && !selectedCountry && (
+                <CountriesList countries={filteredCountries} handleShowCountry={handleShowCountry} />
+            )}
+            {selectedCountry && (
+                <CountryDetail country={selectedCountry} />
+            )}
         </div>
     )
 }
