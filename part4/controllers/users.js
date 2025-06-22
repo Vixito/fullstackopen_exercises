2acca1662d6afd6c1fad7b1b09f7bee94eb0665f
Vixito <carlosandres81182@gmail.com>
2025-06-22 16:08:28 -0500

    Exercise Part 4.16 Full Stack Open

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -12,6 +12,19 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (!username || username.length < 3) {
+    return response.status(400).json({ error: 'Username must be at least 3 characters long' })
+  }
+
+  if (!password || password.length < 3) {
+    return response.status(400).json({ error: 'Password must be at least 3 characters long' })
+  }
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'Username must be unique' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
