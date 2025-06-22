738f43dc4c5aa49bfb25d29de1f118cee7bc537d
Vixito <carlosandres81182@gmail.com>
2025-06-22 01:54:07 -0500

    Exercise Part 4.11 Full Stack Open

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -57,6 +57,23 @@ test('a valid blog can be added', async () => {
   expect(titles).toContain('Nuevo Blog')
 })
 
+test('if likes property is missing from request, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'Blog sin likes',
+    author: 'Autor sin likes',
+    url: 'http://nolikes.com'
+    // likes is not defined
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body.likes).toBe(0)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
