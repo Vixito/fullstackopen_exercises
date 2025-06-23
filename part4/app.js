73d5e04f4d5fde3f2f1b570e9aedf7b7dd7cd231
Vixito <carlosandres81182@gmail.com>
2025-06-22 20:44:39 -0500

    Exercise Part 4.18 Full Stack Open

--- part4/app.js
+++ part4/app.js
@@ -8,10 +8,12 @@ mongoose.connect(process.env.MONGODB_URI)
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
