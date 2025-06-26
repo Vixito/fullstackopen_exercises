b49d4f7c8a81e7d06f6bc20413af2b260d77b1e1
Vixito <carlosandres81182@gmail.com>
2025-06-26 14:12:21 -0500

    Exercise Part 5.7 Full Stack Open

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    padding: 10,
+    border: '1px solid #ccc',
+    marginBottom: 5,
+    borderRadius: 4,
+    background: '#f9f9f9'
+  }
+
+  return (
+    <div style={blogStyle} className="blog">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+      {showDetails && (
+        <div className="blogDetails">
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}</div>
+          <div>{blog.user && blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
