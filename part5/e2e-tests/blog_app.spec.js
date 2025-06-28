fc3a24d07b0e195d6b58ac9d9948e8b0daf883f8
Vixito <carlosandres81182@gmail.com>
2025-06-28 13:47:05 -0500

    Exercise Part 5.18 Full Stack Open

--- part5/e2e-tests/blog_app.spec.js
+++ part5/e2e-tests/blog_app.spec.js
@@ -1,7 +1,18 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
+const apiUrl = 'http://localhost:3001/api';
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    // Vacía la base de datos
+    await request.post(`${apiUrl}/testing/reset`);
+    // Crea usuario
+    await request.post(`${apiUrl}/users`, {
+      data: {
+        username: 'testuser',
+        name: 'Test User',
+        password: 'testpass'
+      }
+    });
     await page.goto('http://localhost:5173');
   });
 
@@ -11,4 +22,20 @@ describe('Blog app', () => {
     await expect(page.locator('input[name="Username"]')).toBeVisible();
     await expect(page.locator('input[name="Password"]')).toBeVisible();
   });
-});
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('testuser');
+      await page.locator('input[name="Password"]').fill('testpass');
+      await page.getByRole('button', { name: 'login' }).click();
+      await expect(page.getByText(/logged in/i)).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.locator('input[name="Username"]').fill('testuser');
+      await page.locator('input[name="Password"]').fill('wrongpass');
+      await page.getByRole('button', { name: 'login' }).click();
+      await expect(page.getByText(/wrong credentials/i)).toBeVisible();
+    });
+  });
+})
