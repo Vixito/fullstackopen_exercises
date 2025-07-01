be43e6988d9bdda177e3b8ebc4580aaca45aa081
Vixito <carlosandres81182@gmail.com>
2025-06-30 23:45:12 -0500

    Exercise Part 6.22 Full Stack Open

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,14 +1,31 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './services/anecdotes'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './services/anecdotes'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
   const { data, isLoading, isError } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
     retry: 1 // o false si no quiero reintentos
   })
+
+  const voteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      queryClient.setQueryData(['anecdotes'], (oldAnecdotes) =>
+        oldAnecdotes.map(a =>
+          a.id === updatedAnecdote.id ? updatedAnecdote : a
+        )
+      )
+    }
+  })
+
+  const handleVote = (anecdote) => {
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  }
 
   if (isLoading) {
     return <div>loading data...</div>
@@ -28,7 +45,7 @@ const App = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button>vote</button>
+            <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
       )}
