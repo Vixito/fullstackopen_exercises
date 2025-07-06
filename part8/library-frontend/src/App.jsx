97db9d66fe5b83e05d58e282451688ee44b69da3
Vixito <carlosandres81182@gmail.com>
2025-07-06 01:29:08 -0500

    Exercise Part 8.18 Full Stack Open

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("library-user-token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    setPage("authors");
+  };
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {token && <button onClick={() => setPage("add")}>add book</button>}
+        {!token ? (
+          <button onClick={() => setPage("login")}>login</button>
+        ) : (
+          <button onClick={logout}>logout</button>
+        )}
       </div>
 
       <Authors show={page === "authors"} />
@@ -19,6 +39,12 @@ const App = () => {
       <Books show={page === "books"} />
 
       <NewBook show={page === "add"} />
+
+      <LoginForm
+        show={page === "login"}
+        setToken={setToken}
+        setPage={setPage}
+      />
     </div>
   );
 };
